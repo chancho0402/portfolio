e4276ec640155965c8f4cdd1a093fd39cc6e4d28
chanchala bharti <khushibharti6354@gmail.com>
2025-08-07 00:56:13 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,11 @@
+let gameNum = 23;
+let userNum = prompt("guess the number: ");
+while( userNum != gameNum){
+  userNum = prompt ("you entered wrong number.guess again:");
+
+}
+//console.log("congratulation AMIT , you entered right number");//
+
+//let fullName = prompt ("enter your  name without space")
+//let username = "@"+ fullName+"_"+ fullName.length;
+//console.log(username);//
